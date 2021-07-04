edd6a04e3b5c1778a602ecaa838c6cb0c15b9949
Sayed Mosaad <sayed.mosaad920@gmail.com>
2021-07-04 13:46:22 +0200

    commit implementation of service and repository GetCourses methods

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domains.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    class CourseService : ICourseService
+    {
+        private ICourseRepository _Courserepository;
+        public CourseService(ICourseRepository Courserepository)
+        {
+            _Courserepository = Courserepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _Courserepository.GetCourses()
+            };
+        }
+    }
+}
