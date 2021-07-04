af708981a0a82074866b4d7738429b4dbd20a7b8
Sayed Mosaad <sayed.mosaad920@gmail.com>
2021-07-04 13:32:57 +0200

    Commit Course Services and Interfaces and course ViewModel

---
+++ CleanArch/CleanArch.Domains/Interfaces/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domains.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domains.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
